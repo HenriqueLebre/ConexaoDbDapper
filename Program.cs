49087089d90e35dd9b846919e1b2562b4e2c82bb
henriqueLebre <henrique_lebre@hotmail.com>
2023-08-23 22:41:37 -0300

    CRUD em banco de dados.

--- Program.cs
+++ Program.cs
@@ -25,5 +25,61 @@ namespace ConexaoBDDapper
                 Console.WriteLine($"{category.Id} - {category.Title}");
             }
         }
+
+        static void GetCategory(SqlConnection conn)
+        {
+            var category = conn
+                .QueryFirstOrDefault<Category>(
+                    "SELECT TOP 1 [Id], [Title] FROM [Category] WHERE [Id]=@id",
+                    new
+                    {
+                        id = ""
+                    }
+                );
+            Console.WriteLine($"{category.Id} - {category.Title}");
+        }
+
+        static void CreateCategory(SqlConnection conn)
+        {
+            var category = new Category();
+            category.Id = Guid.NewGuid();
+            category.Title = "AMAZON AWS";
+
+            var insertSql = "INSERT INTO [Category] VALUES(@Id, @Title)";
+
+            var rows = conn.Execute(insertSql, new
+            {
+                category.Id,
+                category.Title
+            });
+
+            Console.WriteLine($"{rows} quantidade de registros inseridos.");
+        }
+
+        static void UpdateCategory(SqlConnection conn)
+        {
+            var updateQuery = "UPDATE [Category] SET [Title]=@Title WHERE [Id]=@id";
+            var rows = conn.Execute(updateQuery, new
+            {
+                id = new Guid(""),
+                title = "DevSecOps"
+            });
+
+            Console.WriteLine($"{rows} quantidade de registro atualizados.");
+
+        }
+
+        static void DeleteCategory(SqlConnection conn)
+        {
+            var deleteQuery = "DELETE [Category] WHERE [id]=@id";
+            var rows = conn.Execute(deleteQuery, new
+            {
+                id = new Guid("")
+            });
+
+            Console.WriteLine($"{rows} quantidade de registros deletados.");
+
+        }
+
     }
 }
